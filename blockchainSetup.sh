b1188974dfec0bef10f3a1f5dceb8497e1936fb0
Ashutosh Singh <33693028+singhashutosh96@users.noreply.github.com>
2019-03-29 15:37:29 +0530

    Add files via upload

---
+++ blockchainSetup.sh
@@ -0,0 +1,10 @@
+sudo apt update && sudo apt upgrade 
+sudo apt install -y net-tools curl python3-pip build-essential git
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install -y nodejs
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile 
+source ~/.profile
+npm i -g truffle 
+
